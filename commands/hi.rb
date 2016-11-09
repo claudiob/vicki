390081996bf4b5802365524ff9b0457133fd783b
claudiob <claudiob@inventati.org>
2016-11-09 09:25:27 -0800

    Add smarter Yt commands

--- commands/hi.rb
+++
@@ -1,9 +0,0 @@
-module Vicki
-  module Commands
-    class Hi < SlackRubyBot::Commands::Base
-      command 'hi' do |client, data, _match|
-        client.say channel: data.channel, text: 'hi'
-      end
-    end
-  end
-end
